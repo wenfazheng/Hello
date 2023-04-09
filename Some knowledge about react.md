959d82ab2cab669c7a7de7363adc99273bd8deb6
wenfa.zheng <129184384+wenfazheng@users.noreply.github.com>
2023-04-10 01:04:28 +0800

    4-10

--- Some knowledge about react.md
+++ Some knowledge about react.md
@@ -1,1 +1,15 @@
 react中的三个点...叫做展开运算符
+
+
+
+学习react的一些资料：
+
+B站视频：[课程先导_哔哩哔哩_bilibili](https://www.bilibili.com/video/BV1Z44y1K7Fj?p=1&vd_source=b0dce3e52469efe74d69bd73b7b892ab)
+
+源码地址：https://gitee.com/react-cp/react-pc-code 
+
+React基础讲义: https://www.yuque.com/fechaichai/qeamqf/xbai87 
+
+React和Mobx讲义: https://www.yuque.com/fechaichai/qeamqf/apomum 
+
+ReactPc项目讲义: https://www.yuque.com/fechaichai/tzzlh1
--- react-basic/src/13组件通信父串子.jsx
+++ react-basic/src/13组件通信父串子.jsx
@@ -2,29 +2,26 @@ import './App.css'
 import React from 'react'
 
 //3.接收数据， props是一个对象 里面存着通过父组件传入的所有数据
-function SonF (props) {
-  return (
-    <div>我是函数子组件,{props.msg}</div>
-  )
+function SonF(props) {
+  return <div>我是函数子组件,{props.msg}</div>
 }
 
-
+//子组件
 //Component的C一定要大写
 class SonC extends React.Component {
-  render () {
-    return (
-      <div>我是类子组件,{this.props.msg}</div>
-    )
+  render() {
+    return <div>我是类子组件,{this.props.msg}</div>
   }
 }
 
+//父组件
 class App extends React.Component {
   //1.准备数据
   state = {
-    message: 'this is a message'
+    message: 'this is a message',
   }
   //2.绑定数据
-  render () {
+  render() {
     return (
       <div>
         <SonC msg={this.state.message} />
@@ -33,6 +30,5 @@ class App extends React.Component {
     )
   }
 }
-
 
 export default App
